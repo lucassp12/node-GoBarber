bb5705fade0651b0374e0bcebf9bab265f386e0d
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-03-31 16:51:45 -0300

    Adicionando agendamento de serviço

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -5,6 +5,7 @@ const { Appointment } = require("../models");
 class AvailableController {
   async index(req, res) {
     const date = moment(parseInt(req.query.date));
+
     const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.provider,
@@ -16,9 +17,10 @@ class AvailableController {
         }
       }
     });
+
     const schedule = [
-      "08:00",
-      "09:00",
+      "8:00",
+      "9:00",
       "10:00",
       "11:00",
       "12:00",
@@ -29,21 +31,23 @@ class AvailableController {
       "17:00",
       "18:00"
     ];
+
     const available = schedule.map(time => {
       const [hour, minute] = time.split(":");
       const value = date
         .hour(hour)
-        .minute("minute")
+        .minute(minute)
         .second(0);
 
       return {
         time,
         value: value.format(),
-        avaiable:
+        available:
           value.isAfter(moment()) &&
           !appointments.find(a => moment(a.date).format("HH:mm") === time)
       };
     });
+
     return res.render("available/index", { available });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multerConfig = require("./config/multer");
 const upload = require("multer")(multerConfig);
+
 const routes = express.Router();
 
 const authMiddleware = require("./app/middlewares/auth");
@@ -14,10 +15,10 @@ const AppointmentController = require("./app/controllers/AppointmentController")
 const AvailableController = require("./app/controllers/AvailableController");
 
 routes.use((req, res, next) => {
-  res.locals.flashSucess = req.flash("success");
+  res.locals.flashSucces = req.flash("success");
   res.locals.flashError = req.flash("error");
 
-  return next();
+  next();
 });
 
 routes.get("/files/:file", FileController.show);
@@ -26,7 +27,7 @@ routes.get("/", guestMiddleware, SessionController.create);
 routes.post("/signin", SessionController.store);
 
 routes.get("/signup", guestMiddleware, UserController.create);
-routes.post("/signup", upload.single("avatar"), UserController.store);
+routes.post("/singup", upload.single("avatar"), UserController.store);
 
 routes.use("/app", authMiddleware);
 
@@ -35,6 +36,7 @@ routes.get("/app/logout", SessionController.destroy);
 routes.get("/app/dashboard", DashboardController.index);
 
 routes.get("/app/appointments/new/:provider", AppointmentController.create);
+routes.post("/app/appointments/new/:provider", AppointmentController.store);
 routes.get("/app/available/:provider", AvailableController.index);
 
 module.exports = routes;
